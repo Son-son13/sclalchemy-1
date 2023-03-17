6f155f83ff15cb68ed7d62386fbce325e686d1f2
Son-son13 <118826588+Son-son13@users.noreply.github.com>
2023-03-17 18:47:31 +0300

    Add files via upload

---
+++ data/jobs..py
@@ -0,0 +1,23 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+
+from .db_session import SqlAlchemyBase
+
+
+class News(SqlAlchemyBase):
+    __tablename__ = 'news'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    team_leader = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    job = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    work_size = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    collaborators = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    start_date = sqlalchemy.Column(sqlalchemy.DateTime, nullable=True)
+    end_date = sqlalchemy.Column(sqlalchemy.DateTime, nullable=True)
+    is_finished = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("users.id"))
+    user = orm.relationship('User')
+
+
